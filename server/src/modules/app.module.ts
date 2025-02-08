b212d9ff095bdf7e03d50cb055650e73a3ecd80b
Philix27 <philixbob@gmail.com>
2025-02-09 00:19:39 +0100

    install nestjs base template

---
+++ server/src/modules/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+
+import { CommonModule } from "./common";
+import { PassengerModule } from "./passenger/passenger.module";
+
+@Module({
+    imports: [CommonModule, PassengerModule],
+})
+export class ApplicationModule {}
